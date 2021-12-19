5864def8186dcb867ca44b2a414a3545be68d76a
dunger <belov.dv@phystech.edu>
2021-12-20 00:08:12 +0300

    ci test error

--- code/suffix_automat/test.cpp
+++ code/suffix_automat/test.cpp
@@ -130,6 +130,11 @@ TEST(Substrings, all)
 
 // ===== // ===== // ===== // ===== // ===== // ===== // ===== // ===== //
 
+TEST(CI, error)
+{
+    ASSERT_EQ(1, 0);
+}
+
 int main(int argc, char **argv)
 {
     srand(0);
